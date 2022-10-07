97abcef8ae213b676522ee9be65d967c47188205
pasindulakshankudaligama <pasindulakshankudaligama@gmail.com>
2022-10-07 17:29:35 +0530

    register s screen added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Login from './screens/Login'
+import Register from './screens/Register';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -14,6 +15,7 @@ export default function App() {
           headerShown: false,
         }}>
         <Stack.Screen name="Login" component={Login} />
+ 	<Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
     </NavigationContainer>
   
