41d48cf02e81a05e9e16562762c6eb9447c2734f
pasindulakshankudaligama <pasindulakshankudaligama@gmail.com>
2022-10-07 18:13:22 +0530

    home screen added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Login from './screens/Login'
 import Register from './screens/Register';
+import Home from './screens/Home';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -14,8 +15,9 @@ export default function App() {
         <Stack.Navigator screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Login" component={Login} />
  	<Stack.Screen name="Register" component={Register} />
+	<Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
     </NavigationContainer>
   
