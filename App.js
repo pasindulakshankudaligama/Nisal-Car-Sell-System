52d70648891325d49c089e77b6b22aa8413914c0
pasindulakshankudaligama <pasindulakshankudaligama@gmail.com>
2022-10-07 21:48:10 +0530

    vehicle info screen added

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Login from './screens/Login'
 import Register from './screens/Register';
 import Home from './screens/Home';
 import AddNewVehicleInfo from './screens/AddNewVehicleInfo';
+import VehicleInfo from './screens/VehicleInfo';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -19,6 +20,7 @@ export default function App() {
     	<Stack.Screen name="Register" component={Register} />
 	    <Stack.Screen name="Home" component={Home} />
  <Stack.Screen name="AddNewVehicleInfo" component={AddNewVehicleInfo} />
+ <Stack.Screen name="VehicleInfo" component={VehicleInfo} />
     </Stack.Navigator>
     </NavigationContainer>
   
